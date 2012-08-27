beb2d75593b4dc96b1da789b6b7027123bdd8b5c
Ben Schmeckpeper <ben@aisle50.com>
2012-08-27 08:10:29 -0500

    Toggle colors on good guy/bad guy change.

--- grid.js
+++ grid.js
@@ -1,5 +1,9 @@
 function grid_init() {
     $(".good_guy_choice").click(function(e){
-        alert($(this));
+        $(".scorebox.win1, .scorebox.loss1").toggleClass("win1 loss1");
+        $(".scorebox.win2, .scorebox.loss2").toggleClass("win2 loss2");
+        $(".scorebox.win3, .scorebox.loss3").toggleClass("win3 loss3");
+        $(".scorebox.win4, .scorebox.loss4").toggleClass("win4 loss4");
+        $(".good_guy_choice").toggleClass("good_guy bad_guy");
     });
 }
