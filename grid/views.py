fe9099c7f1c295c798848efe9e31201c6f305374
Ben Schmeckpeper <ben@aisle50.com>
2012-09-06 08:14:07 -0500

    Compute Ben and Brian's teams on start up.

--- grid/views.py
+++ grid/views.py
@@ -5,10 +5,24 @@ from django.template.loader import get_template
 
 import json
 
+from models import Game, Week
+
 def index(request):
+    ben_teams = []
+    brian_teams = []
+
+    wk = Week.objects.all()[0];
+    for game in wk.games_set():
+        if game.picker == "BEN":
+            ben_teams.append(game.picked_team)
+        else:
+            brian_teams.append(game.picked_team)
     interval = 1 * 60 * 1000
     return render_to_response('grid/index.html',
-                              {'interval': interval},
+                              {'ben_teams': json.dumps(ben_teams),
+                               'brian_teams': json.dumps(brian_teams),
+                               'interval': interval
+                               },
                               context_instance=RequestContext(request))
 
 def scores(request):
