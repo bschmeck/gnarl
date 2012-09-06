d36906c5a03320ea3c3f1b3afe97e2e33614d84b
Ben Schmeckpeper <ben.schmeckpeper@gmail.com>
2012-09-05 21:56:23 -0500

    Fix bugs in urls.py.

--- urls.py
+++ urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls.defaults import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
-grid_patterns = patters('',
+grid_patterns = patterns('',
     url(r'^$', 'grid.views.index'),
     url(r'scores/$', 'grid.views.scores')
 )
