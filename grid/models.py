9552aa3ffcec736ecba730e83db2dc91b7c9e24c
Ben Schmeckpeper <ben.schmeckpeper@gmail.com>
2012-09-05 21:54:54 -0500

    First pass at Django models.

---
+++ grid/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+from datetime import date
+
+class Week(models.Model):
+    start_date = models.DateField('Date')
+    number = models.IntegerField()
+    scoreboard_url = models.CharField(max_length=255)
+
+    def __unicode__(self):
+        return "Week %d" % self.number
+
+class Game(models.Model):
+    PICKER_CHOICES = (
+        ("BEN", "Ben"),
+        ("BRIAN", "Brian"),
+    )
+    home_team = models.CharField(max_length=3)
+    home_score = models.IntegerField()
+    away_team = models.CharField(max_length=3)
+    away_score = models.IntegerField()
+
+    week = models.ForeignKey(Week)
+
+    picked_team = models.CharField(max_length=3)
+    picker = models.CharField(max_length=5, choices=PICKER_CHOICES)
+
+    def __unicode__(self):
+        return "%s @ %s - Week %d" % (self.away_team, self.home_team, self.week.number)
