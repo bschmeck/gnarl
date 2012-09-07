44fa0e61453ecff74ebcbd4a7d49bea9851dd94d
Ben Schmeckpeper <ben@aisle50.com>
2012-09-06 23:54:59 -0500

    Django model changes.

--- grid/models.py
+++ grid/models.py
@@ -24,6 +24,10 @@ class Game(models.Model):
 
     picked_team = models.CharField(max_length=3)
     picker = models.CharField(max_length=5, choices=PICKER_CHOICES)
+    lock = models.BooleanField(default=False)
+    anti_lock = models.BooleanField(default=False)
+
+    time_left = models.CharField(max_length=100)
 
     def __unicode__(self):
         return "%s @ %s - Week %d" % (self.away_team, self.home_team, self.week.number)
