b992f27122d0fedb1b067e99790ad7ab7169e458
Ben Schmeckpeper <ben.schmeckpeper@gmail.com>
2012-09-08 00:58:42 -0500

    Need to serialize games to JSON, can't just dump them.

--- grid/views.py
+++ grid/views.py
@@ -1,3 +1,4 @@
+from django.core import serializers
 from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseForbidden
 from django.shortcuts import get_object_or_404, redirect, render_to_response
 from django.template import RequestContext
@@ -32,4 +33,5 @@ def index(request):
 def scores(request):
     wk = Week.objects.latest()
     games = wk.game_set.all()
-    return HttpResponse(json.dumps(games), "application/javascript")
+    ret = serializers.serialize('json', games)
+    return HttpResponse(ret, "application/javascript")
